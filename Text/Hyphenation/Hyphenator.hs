b6b61c4dbfcc0d1fa25923bab188f5e53a164b45
Edward Kmett <ekmett@gmail.com>
2012-02-11 03:38:03 -0500

    cleaned up termination criterion

--- Text/Hyphenation/Hyphenator.hs
+++ Text/Hyphenation/Hyphenator.hs
@@ -9,13 +9,9 @@
 -- Portability :  portable
 --
 -- Hyphenation based on the Knuth-Liang algorithm as used by TeX.
---
--- The implementation is based on Ned Batchelder's public domain @hyphenate.py@
--- and simplified to remove the need for a manual exception list.
 ----------------------------------------------------------------------------
 module Text.Hyphenation.Hyphenator
   ( Hyphenator(..)
-  , hyphenationScore
   -- * Hyphenate with a given set of patterns
   , hyphenate
   , defaultLeftMin
@@ -30,11 +26,11 @@ defaultLeftMin = 2
 defaultRightMin = 3
 
 data Hyphenator = Hyphenator
-  { hyphenatorChars      :: Char -> Char
-  , hyphenatorPatterns   :: Patterns
-  , hyphenatorExceptions :: Exceptions
-  , hyphenatorLeftMin    :: {-# UNPACK #-} !Int
-  , hyphenatorRightMin   :: {-# UNPACK #-} !Int
+  { hyphenatorChars      :: Char -> Char        -- ^ a normalization function applied to input characters before applying patterns or exceptions
+  , hyphenatorPatterns   :: Patterns            -- ^ hyphenation patterns stored in a trie
+  , hyphenatorExceptions :: Exceptions          -- ^ exceptions to the general hyphenation rules, hyphenated manually
+  , hyphenatorLeftMin    :: {-# UNPACK #-} !Int -- ^ the number of characters as the start of a word to skip hyphenating, by default: 2
+  , hyphenatorRightMin   :: {-# UNPACK #-} !Int -- ^ the number of characters at the end of the word to skip hyphenating, by default: 3
   }
 
 hyphenationScore :: Hyphenator -> String -> [Int]
@@ -44,14 +40,20 @@ hyphenationScore (Hyphenator nf ps es l r) s
     Just pts -> trim pts
     Nothing -> trim (lookupPattern ls ps)
   where
-    trim result = replicate l 0 ++ take (n - l - r) (drop l result) ++ replicate r 0 -- drop the final replicate?
+    trim result = replicate l 0 ++ take (n - l - r) (drop l result)
     n  = length s
     ls = map nf s
 
+-- | hyphenate a single word using the specified Hyphenator. Returns a set of candidate breakpoints by decomposing the input
+-- into substrings.
+-- 
+-- > ghci> hyphenate english_US "supercalifragilisticexpialadocious"
+-- > ["su","per","cal","ifrag","ilis","tic","ex","pi","al","ado","cious"]
+-- > ghci> hyphenate english_US "hyphenation"
+-- > ["hy","phen","ation"]
 hyphenate :: Hyphenator -> String -> [String]
 hyphenate h s0 = go [] s0 $ tail $ hyphenationScore h s0 where
   go acc (w:ws) (p:ps)
     | odd p     = reverse (w:acc) : go [] ws ps
     | otherwise = go (w:acc) ws ps
-  go acc [] _ = [reverse acc]
-  go acc ws [] = [reverse acc ++ ws]
+  go acc ws _  = [reverse acc ++ ws]
